1b0c7a728aba7beb6f6588809c7fc9b0608507e2
Renata Fraga <Renata Fraga>
2017-06-29 18:26:34 -0300

    Inclusao de modal add postagem Ainda nao esta em execucao Implementados html e ts deste componente

--- vida-de-livreiro-beta/src/app/administrador/paineladdpostagem/paineladdpostagem.component.ts
+++ vida-de-livreiro-beta/src/app/administrador/paineladdpostagem/paineladdpostagem.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { MzBaseModal } from "ng2-materialize/dist";
+import { PostagemecomentarioService } from "app/postagemecomentario.service";
 
 @Component({
   selector: 'app-paineladdpostagem',
   templateUrl: './paineladdpostagem.component.html',
   styleUrls: ['./paineladdpostagem.component.css']
 })
-export class PaineladdpostagemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class PaineladdpostagemComponent  extends MzBaseModal implements OnInit{
+  // postagem: Postagem;
+  constructor(private servico: PostagemecomentarioService) {
+     super();
   }
 
+  ngOnInit() {
+   // postagem = new Postagem();
+  }
+
+  /*salvarPostagem() {
+		if(this.postagem.titulo == null) {
+			alert("O campo título é de preenchimento obrigatório.");
+		} else {
+			if(this.postagem.texto == null) {
+				alert("O campo texto é de preenchimento obrigatório.");
+			} else {
+				this.servico.adicionarPostagem(this.postagem);
+				alert("Postagem incluída com sucesso! ");
+			}
+		}
+	}*/
 }
